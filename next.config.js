e8fbe1c3454f487fe26097d15717728a53e5efbc
comolove <jungjinwoo41@gmail.com>
2023-07-25 15:22:38 +0900

    build: add dockerfile and downgrade next version

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'standalone',
+    experimental: {
+        appDir: true,
+    },
+}
 
 module.exports = nextConfig
