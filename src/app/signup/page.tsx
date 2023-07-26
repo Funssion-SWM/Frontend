2b8c4487cabb03f23e344de1218f54e9ae5039e1
dongree <dongwoo0307@naver.com>
2023-07-27 01:10:23 +0900

    Style: Signup UI 수정

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,9 +3,9 @@ import Link from 'next/link';
 
 export default function SignupPage() {
   return (
-    <section className="flex flex-col w-full items-center max-w-screen-sm mx-auto my-10">
-      <h1 className="text-2xl font-bold">LOGO</h1>
-      <Link href="/login" className="self-end my-2 font-bold">
+    <section className="flex flex-col w-full items-center max-w-screen-sm mx-auto mt-5 shadow-lg py-5 px-32">
+      <h1 className="text-3xl font-bold mb-5">SOMA</h1>
+      <Link href="/login" className="self-end my-2 font-semibold">
         로그인
       </Link>
       <SignupForm />
--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -80,112 +80,110 @@ export default function SignupForm() {
     confirmCode(signupData.authCode).then(console.log).catch(console.error);
   };
 
+  const INPUT_STYLE =
+    'text-lg border-2 my-2 py-2 px-4 rounded-lg bg-soma-grey-20 border-soma-grey-30 grow';
+  const GREY_BTN_STYLE =
+    'bg-soma-grey-30 rounded-2xl mx-2 px-2 text-soma-grey-45 font-semibold text-lg py-2';
+
   return (
     <form className="flex flex-col w-full" onSubmit={handleSubmit}>
-      <div className="flex flex-col my-2">
-        <label htmlFor="email" className="text-lg">
+      <div className="flex flex-col my-1">
+        <label htmlFor="email" className="text-sm">
           이메일
         </label>
-        <div className="flex">
+        <div className="flex items-center">
           <input
-            className="text-2xl border-2 grow"
+            className={INPUT_STYLE}
             type="email"
             id="email"
             name="email"
             value={signupData.email}
             onChange={handleChange}
             required
+            placeholder="이메일을 입력해주세요."
           />
-          <button
-            className="bg-green-400 rounded-lg mx-2 px-2 font-bold"
-            onClick={handleIsDuplicate1}
-          >
+          <button className={GREY_BTN_STYLE} onClick={handleIsDuplicate1}>
             중복확인
           </button>
-          <button
-            className="bg-green-400 rounded-lg mx-2 px-2 font-bold"
-            onClick={handleSendCode}
-          >
+          {/* <button className={GREY_BTN_STYLE} onClick={handleSendCode}>
             보내기
-          </button>
+          </button> */}
         </div>
       </div>
       {msgFlag1 && <WarningMessage text="중복되는 이메일입니다." />}
-      <div className="flex flex-col my-2">
-        <label htmlFor="authCode" className="text-lg">
+      <div className="flex flex-col my-1">
+        <label htmlFor="authCode" className="text-sm">
           인증번호
         </label>
-        <div className="flex">
+        <div className="flex items-center">
           <input
-            className="text-2xl border-2 grow"
+            className={INPUT_STYLE}
             type="text"
             id="authCode"
             name="authCode"
             value={signupData.authCode}
             onChange={handleChange}
             required
+            placeholder="인증번호 6자리를 입력해주세요."
           />
-          <button
-            className="bg-green-400 rounded-lg mx-2 px-2 font-bold"
-            onClick={handleConfirmCode}
-          >
-            확인
+          <button className={GREY_BTN_STYLE} onClick={handleConfirmCode}>
+            인증
           </button>
         </div>
       </div>
-      <div className="flex flex-col  my-2">
-        <label htmlFor="nickname" className="text-lg">
+      <div className="flex flex-col my-1">
+        <label htmlFor="nickname" className="text-sm">
           닉네임
         </label>
-        <div className="flex">
+        <div className="flex items-center">
           <input
-            className="text-2xl border-2 grow"
+            className={INPUT_STYLE}
             type="text"
             id="nickname"
             name="nickname"
             value={signupData.nickname}
             onChange={handleChange}
             required
+            placeholder="활동할 닉네임을 입력해주세요."
           />
-          <button
-            className="bg-green-400 rounded-lg mx-2 px-2 font-bold"
-            onClick={handleIsDuplicate2}
-          >
+          <button className={GREY_BTN_STYLE} onClick={handleIsDuplicate2}>
             중복확인
           </button>
         </div>
       </div>
       {msgFlag2 && <WarningMessage text="중복되는 닉네임입니다." />}
-      <div className="flex flex-col  my-2">
-        <label htmlFor="pw" className="text-lg">
+      <div className="flex flex-col  my-1">
+        <label htmlFor="pw" className="text-sm">
           비밀번호
         </label>
         <input
-          className="text-2xl border-2"
+          className={INPUT_STYLE}
           type="password"
           id="pw"
           name="pw"
           value={signupData.pw}
           onChange={handleChange}
           required
+          placeholder="비밀번호를 입력해주세요."
         />
       </div>
-      <div className="flex flex-col  my-2">
-        <label htmlFor="confirmPw" className="text-lg">
+      <div className="flex flex-col  my-1">
+        <label htmlFor="confirmPw" className="text-sm">
           비밀번호 확인
         </label>
         <input
-          className="text-2xl border-2"
+          className={INPUT_STYLE}
           type="password"
           id="confirmPw"
           name="confirmPw"
           value={signupData.confirmPw}
           onChange={handleChange}
           required
+          placeholder="입력한 비밀번호를 다시 입력해주세요."
         />
       </div>
       {msgFlag3 && <WarningMessage text="비밀번호가 같지 않습니다." />}
-      <button className="bg-green-400 rounded-lg py-2 my-5 font-bold">
+      <button className="bg-soma-blue-40 text-soma-white rounded-3xl py-3 my-5 font-bold">
         회원가입
       </button>
     </form>
