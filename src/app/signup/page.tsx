a55692bcf337e2424087a13ae8e554be854acb89
dongree <dongwoo0307@naver.com>
2023-07-28 02:14:03 +0900

    Style: 로그인, 회원가입 UI 수정

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -9,8 +9,11 @@ export const metadata: Metadata = {
 
 export default function SignupPage() {
   return (
-    <section className="flex flex-col w-full items-center max-w-screen-sm mx-auto mt-5 shadow-lg py-5 px-32">
-      <h1 className="text-3xl font-bold mb-5">Inforum</h1>
+    <section className="relative flex flex-col w-full items-center max-w-screen-sm mx-auto mt-12 shadow-lg py-5 px-32">
+      <Link href="/" className="absolute top-2 right-4 text-soma-grey-50">
+        메인으로
+      </Link>
+      <h1 className="text-3xl font-bold my-5">Inforum</h1>
       <Link href="/login" className="self-end my-2 font-semibold">
         로그인
       </Link>
