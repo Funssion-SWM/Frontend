329b40ec9c4ee20ea4abbea63da79458ecc82348
dongree <dongwoo0307@naver.com>
2023-08-07 16:22:16 +0900

    Style: button hover 적용

--- src/components/shared/BlueBtn.tsx
+++ src/components/shared/BlueBtn.tsx
@@ -7,7 +7,7 @@ type Props = {
 export default function BlueBtn({ text, onClick, extraStyle = '' }: Props) {
   return (
     <button
-      className={`bg-soma-blue-40 text-white px-3.5 py-2 rounded-3xl ${extraStyle}`}
+      className={`bg-soma-blue-40 text-white px-3.5 py-2 rounded-3xl ${extraStyle} transition hover:bg-soma-blue-50`}
       onClick={() => onClick()}
     >
       {text}
