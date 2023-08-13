b196360a7376e47d68e1c09645f3748ef1d88c3f
dongree <dongwoo0307@naver.com>
2023-08-13 15:57:00 +0900

    Chore: key에 NEXT_PUBLIC 추가

--- src/app/api/generate/route.ts
+++ src/app/api/generate/route.ts
@@ -2,7 +2,7 @@ import { Configuration, OpenAIApi } from 'openai-edge';
 import { OpenAIStream, StreamingTextResponse } from 'ai';
 
 const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(config);
 
