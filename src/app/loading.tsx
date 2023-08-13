b53b6ed5cb22b4a88a174420f91bd6ee17f99820
dongree <dongwoo0307@naver.com>
2023-08-13 18:25:03 +0900

    Chore: Loading 추가

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import { BeatLoader } from 'react-spinners';
+
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <BeatLoader color="#4992FF" />
+    </div>
+  );
+}
