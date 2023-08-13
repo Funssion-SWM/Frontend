e8099ac9cab4317468a22bd8eb32cfea7748a62c
dongree <dongwoo0307@naver.com>
2023-08-14 01:14:55 +0900

    Chore: memo text placeholder 수정, open ai prompt 수정

--- src/components/ui/editor/extensions/index.ts
+++ src/components/ui/editor/extensions/index.ts
@@ -106,8 +106,8 @@ export const TiptapExtensions = [
       if (node.type.name === 'heading') {
         return `Heading ${node.attrs.level}`;
       }
-      // return "Press '/' for commands, or '++' for AI autocomplete...";
-      return "Press '/' for commands";
+      return "Press '/' for commands, or '++' for AI autocomplete";
+      // return "Press '/' for commands";
     },
     includeChildren: true,
   }),
