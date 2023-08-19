486458e01a9d0ed38ae642eed0737a1c4c2ff980
dongree <dongwoo0307@naver.com>
2023-08-19 19:18:06 +0900

    Feat: Google Login UI

---
+++ src/components/GoogleLoginBtn.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import Link from 'next/link';
+import googleIcon from '../assets/icons/google_icon.svg';
+
+import Image from 'next/image';
+
+export default function GoogleLoginBtn() {
+  return (
+    <Link href="/api/oauth2/authorization/google" className="w-full">
+      <button
+        className={`flex w-full justify-between items-center px-3.5 py-2 rounded-3xl transition border-2 border-soma-grey-30 hover:bg-soma-grey-20 text-sm sm:text-base`}
+        onClick={() => {}}
+      >
+        <Image src={googleIcon} alt="googleIcon" width={20} height={20} />
+        <p>구글 계정 로그인</p>
+        <div className="w-5 h-5"></div>
+      </button>
+    </Link>
+  );
+}
