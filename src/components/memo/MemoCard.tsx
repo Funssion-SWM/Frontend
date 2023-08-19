a50527d1b3d3a4463ca42510f5a9702302b99867
dongree <dongwoo0307@naver.com>
2023-08-19 21:14:48 +0900

    Fix: memo header image click시 마이프로필 routing

--- src/components/memo/MemoCard.tsx
+++ src/components/memo/MemoCard.tsx
@@ -13,38 +13,39 @@ export default function MemoCard({
     memoDescription,
     memoColor,
     createdDate,
+    authorId,
     authorName,
     authorProfileImagePath,
     likes,
   },
 }: Props) {
   return (
-    <Link href={`/memos/${memoId}`}>
-      <article
-        className={`rounded-md shadow-md aspect-square p-4 ${
-          {
-            yellow: 'bg-memo-yellow',
-            green: 'bg-memo-green',
-            skyblue: 'bg-memo-skyblue',
-            orange: 'bg-memo-orange',
-            pink: 'bg-memo-pink',
-            navy: 'bg-memo-navy',
-            purple: 'bg-memo-purple',
-          }[memoColor]
-        } `}
-      >
-        <MemoCardHeader
-          createDate={createdDate}
-          authorName={authorName}
-          likes={likes}
-          imagePath={authorProfileImagePath}
-        />
-
+    <article
+      className={`rounded-md shadow-md aspect-square p-4 ${
+        {
+          yellow: 'bg-memo-yellow',
+          green: 'bg-memo-green',
+          skyblue: 'bg-memo-skyblue',
+          orange: 'bg-memo-orange',
+          pink: 'bg-memo-pink',
+          navy: 'bg-memo-navy',
+          purple: 'bg-memo-purple',
+        }[memoColor]
+      } `}
+    >
+      <MemoCardHeader
+        createDate={createdDate}
+        authorName={authorName}
+        likes={likes}
+        imagePath={authorProfileImagePath}
+        authorId={authorId}
+      />
+      <Link href={`/memos/${memoId}`}>
         <h2 className="text-2xl font-bold my-5 line-clamp-2 break-all">
           {memoTitle}
         </h2>
         <p className="line-clamp-3 break-all">{memoDescription}</p>
-      </article>
-    </Link>
+      </Link>
+    </article>
   );
 }
--- src/components/memo/MemoCardHeader.tsx
+++ src/components/memo/MemoCardHeader.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image';
 import exampleImg from '../../../public/img/profile.png';
 import fillHeart from '../../assets/icons/heart_fill.svg';
+import Link from 'next/link';
 
 type Props = {
   createDate: string;
   authorName: string;
   likes: number;
   imagePath: string;
+  authorId: number;
 };
 
 export default function MemoCardHeader({
@@ -14,17 +16,20 @@ export default function MemoCardHeader({
   authorName,
   likes,
   imagePath,
+  authorId,
 }: Props) {
   return (
     <div className="flex justify-between items-center">
       <div className="flex items-center">
-        <Image
-          src={imagePath ?? exampleImg}
-          alt="exampleImg"
-          width={40}
-          height={40}
-          className="rounded-full w-10 h-10 object-cover"
-        />
+        <Link href={`/me/${authorId}`}>
+          <Image
+            src={imagePath ?? exampleImg}
+            alt="exampleImg"
+            width={40}
+            height={40}
+            className="rounded-full w-10 h-10 object-cover"
+          />
+        </Link>
         <div className="ml-2">
           <h4>{authorName}</h4>
           <p className="text-xs text-gray-600">{createDate}</p>
