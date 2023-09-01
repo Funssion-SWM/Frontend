54c5e6c614140de1b46b922bab036db43c2bd938
dongree <dongwoo0307@naver.com>
2023-09-02 00:15:30 +0900

    Test: BlueBtn test code 수정

--- src/components/shared/btn/tests/BlueBtn.test.tsx
+++ src/components/shared/btn/tests/BlueBtn.test.tsx
@@ -4,9 +4,23 @@ import BlueBtn from '../BlueBtn';
 import userEvent from '@testing-library/user-event';
 
 describe('Blue Btn', () => {
-  it('renders correctly', () => {
+  it('renders correctly when size is small', () => {
     const component = renderer.create(
-      <BlueBtn text="text" onClick={() => {}} />
+      <BlueBtn text="text" size="small" onClick={() => {}} />
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders correctly when size is medium', () => {
+    const component = renderer.create(
+      <BlueBtn text="text" size="medium" onClick={() => {}} />
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders correctly when size is big', () => {
+    const component = renderer.create(
+      <BlueBtn text="text" size="big" onClick={() => {}} />
     );
     expect(component.toJSON()).toMatchSnapshot();
   });
