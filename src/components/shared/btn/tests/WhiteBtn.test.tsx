c5b46f853224f13fa17730c6b87ad6fbe1e35c5b
dongree <dongwoo0307@naver.com>
2023-09-02 13:30:58 +0900

    Test: WhiteBtn 테스트코드 수정

--- src/components/shared/btn/tests/WhiteBtn.test.tsx
+++ src/components/shared/btn/tests/WhiteBtn.test.tsx
@@ -4,9 +4,23 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 describe('BlueBtn2', () => {
-  it('renders correctly', () => {
+  it('renders correctly when size is small', () => {
     const component = renderer.create(
-      <WhiteBtn text="text" onClick={() => {}} />
+      <WhiteBtn text="text" size="small" onClick={() => {}} />
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders correctly when size is medium', () => {
+    const component = renderer.create(
+      <WhiteBtn text="text" size="medium" onClick={() => {}} />
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders correctly when size is big', () => {
+    const component = renderer.create(
+      <WhiteBtn text="text" size="big" onClick={() => {}} />
     );
     expect(component.toJSON()).toMatchSnapshot();
   });
