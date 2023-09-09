07faada50a312aa7a4c3f3ff65be861490e4851c
dongree <dongwoo0307@naver.com>
2023-09-09 10:40:39 +0900

    Fix: search form page에 props 제거

--- src/app/search/form/page.tsx
+++ src/app/search/form/page.tsx
@@ -2,20 +2,12 @@ import CloseIcon from '@/components/search/CloseIcon';
 import SearchForm from '@/components/search/SearchForm';
 import SearchHistory from '@/components/search/SearchHistory';
 
-type Props = {
-  extraClass:string,
-};
-
-export default function SearchFormPage( { extraClass }:Props ) {
-
+export default function SearchFormPage() {
   return (
-    <section className={`flex flex-col max-w-screen-md m-auto ${extraClass}`}>
-     
-      <CloseIcon extraClass='my-7 self-end' />
-
-      <SearchForm/>
-
-      <SearchHistory/>
+    <section className={`flex flex-col max-w-screen-md m-auto`}>
+      <CloseIcon extraClass="my-7 self-end" />
+      <SearchForm />
+      <SearchHistory />
     </section>
-  )
+  );
 }
