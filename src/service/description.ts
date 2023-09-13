9b03b3f96afc3b8e52f2503c59571a7baef2ef97
dongree <dongwoo0307@naver.com>
2023-09-13 14:28:10 +0900

    Fix: description - 모든 text string으로 저장하기

--- src/service/description.ts
+++ src/service/description.ts
@@ -1,20 +1,9 @@
-import { DESCRIPTION_MAX_LENGTH } from '@/utils/const';
-
 export function getDescription(text: string): string {
-  const pattern = /"text":"(.*?)"/g;
+  const pattern = /"text":"(.*?)"}/g;
   const matches = [...text.matchAll(pattern)];
 
-  let description = '';
-
-  if (matches.length > 0) {
-    for (let i = 0; i < matches.length; i++) {
-      if (description.length > DESCRIPTION_MAX_LENGTH) {
-        description = description.substring(0, DESCRIPTION_MAX_LENGTH);
-        break;
-      }
-      if (!matches[i][1].includes('\\n')) description += matches[i][1] + ' ';
-    }
-  }
-
-  return description;
+  return matches.reduce(
+    (description, match) => (description += match[1] + ' '),
+    ''
+  );
 }
