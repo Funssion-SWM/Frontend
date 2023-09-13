900a7591d0bc13c70243a9a8e625d84cfdf946ef
dongree <dongwoo0307@naver.com>
2023-09-13 16:52:27 +0900

    Fix: memo card UI 수정

---
+++ src/components/memo/MemoCardFooter.tsx
@@ -0,0 +1,24 @@
+type Props = {
+  memoTags: string[];
+};
+
+export default function MemoCardFooter({ memoTags }: Props) {
+  return (
+    <div className="flex justify-between items-center text-sm">
+      <div className="flex items-center gap-1 h-10 flex-1 overflow-x-hidden">
+        {memoTags.map((item, idx) => (
+          <div
+            className="font-bold bg-white text-soma-blue-40 rounded-3xl py-1 px-2 line-clamp-1"
+            key={idx}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+      {/* <div className="w-14 text-end">
+          <p className="text-xs text-soma-grey-60">0개의 댓글</p>
+          <p className="text-xs text-soma-grey-60">0개의 질문</p>
+        </div> */}
+    </div>
+  );
+}
