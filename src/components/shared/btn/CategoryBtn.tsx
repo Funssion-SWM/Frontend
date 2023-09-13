b3193b4b18a47ebca6d8609347d0b89a9795b5b2
dongree <dongwoo0307@naver.com>
2023-09-13 22:48:40 +0900

    Fix: 메모사이드바 UI 다듬기, category btn 사이즈 추가

--- src/components/shared/btn/CategoryBtn.tsx
+++ src/components/shared/btn/CategoryBtn.tsx
@@ -1,18 +1,32 @@
+import { ButtonSize } from '@/types';
+
 type Props = {
   text: string;
   onClick: () => void;
+  size?: ButtonSize;
   isSelected: boolean;
 };
 
-export default function CategoryBtn({ text, onClick, isSelected }: Props) {
+export default function CategoryBtn({
+  text,
+  onClick,
+  size = 'medium',
+  isSelected,
+}: Props) {
   return (
     <button
-      className={`border-2 texsomb20 py-2 px-4 rounded-3xl transition text-sm
-          ${
-            isSelected
-              ? 'border-soma-blue-50 text-soma-blue-50 bg-soma-blue-10'
-              : 'border-soma-grey-40 bg-white hover:border-soma-blue-50 hover:text-soma-blue-50 hover:bg-soma-blue-10'
-          }`}
+      className={`border-2 py-2 px-3.5 rounded-3xl transition ${
+        {
+          big: 'text-sm sm:text-base',
+          medium: 'text-xs sm:text-sm',
+          small: 'text-[10px] sm:text-xs',
+        }[size]
+      }
+      ${
+        isSelected
+          ? 'border-soma-blue-50 text-soma-blue-50 bg-soma-blue-10'
+          : 'border-soma-grey-40 bg-white hover:border-soma-blue-50 hover:text-soma-blue-50 hover:bg-soma-blue-10'
+      }`}
       onClick={() => onClick()}
     >
       {text}
