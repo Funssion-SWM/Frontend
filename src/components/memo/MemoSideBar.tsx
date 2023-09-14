7e71c145be0b8724e58a056dea45005d5e66593f
dongree <dongwoo0307@naver.com>
2023-09-14 17:27:19 +0900

    Feat: 메모 페이지 사이드바 접기/펴기 기능 추가, 반응형 적용

--- src/components/memo/MemoSideBar.tsx
+++ src/components/memo/MemoSideBar.tsx
@@ -1,7 +1,12 @@
+'use client';
+
 import CommentsList from '@/components/memo/comment/CommentsList';
 import { Comment } from '@/types/comment';
 import MemoSidebarHeader from './MemoSidebarHeader';
 import CommentForm from '@/components/memo/comment/CommentForm';
+import arrowRight from '@/assets/icons/arrow_right.svg';
+import Image from 'next/image';
+import { useState } from 'react';
 
 type Props = {
   authorName: string;
@@ -20,19 +25,33 @@ export default function MemoSideBar({
   memoId,
   userId,
 }: Props) {
+  const [isVisible, setIsVisible] = useState(true);
+
   return (
-    <aside
-      className={`ml-5 sticky bg-white flex flex-col top-24 rounded-2xl basis-1/4 shadow max-h-for-fit-screen min-w-[300px]`}
-    >
-      <MemoSidebarHeader
-        authorId={authorId}
-        authorName={authorName}
-        authorProfileImagePath={authorProfileImagePath}
-      />
-      <CommentsList comments={comments} userId={userId} />
-      <div className="sticky bottom-0 p-1 bg-white shadow-inner rounded-b-2xl">
-        <CommentForm postId={memoId} />
-      </div>
-    </aside>
+    <div className="sticky top-24 flex max-h-for-fit-screen  ">
+      <button
+        className={`flex z-10 absolute -left-10 sm:-left-3 justify-center items-center opacity-50 w-10 h-10 shadow-inner bg-white rounded-full self-center ${
+          !isVisible && '-scale-x-100'
+        }`}
+        onClick={() => setIsVisible((pre) => !pre)}
+      >
+        <Image src={arrowRight} alt="arrowBtn" />
+      </button>
+      {isVisible && (
+        <aside
+          className={`ml-4 absolute right-0 top-0 sm:static h-full bg-white flex flex-col rounded-2xl shadow min-w-[300px]`}
+        >
+          <MemoSidebarHeader
+            authorId={authorId}
+            authorName={authorName}
+            authorProfileImagePath={authorProfileImagePath}
+          />
+          <CommentsList comments={comments} userId={userId} />
+          <div className="sticky bottom-0 p-1 bg-white shadow-inner rounded-b-2xl">
+            <CommentForm postId={memoId} />
+          </div>
+        </aside>
+      )}
+    </div>
   );
 }
