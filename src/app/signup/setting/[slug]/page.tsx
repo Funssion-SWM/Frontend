313bf1b1017feb127e06f9e725e2e41c7a590b63
dongree <dongwoo0307@naver.com>
2023-09-19 12:15:45 +0900

    Fix: signup관련 logo update

--- src/app/signup/setting/[slug]/page.tsx
+++ src/app/signup/setting/[slug]/page.tsx
@@ -2,6 +2,8 @@ import MyInfoForm from '@/components/MyInfoForm';
 import { redirect } from 'next/navigation';
 import { headers } from 'next/headers';
 import { getDefaultTags } from '@/service/tag';
+import logo from '@/assets/inforum_logo.png';
+import Image from 'next/image';
 
 type Props = {
   params: {
@@ -19,7 +21,7 @@ export default async function SignupSettingPage({ params: { slug } }: Props) {
 
   return (
     <section className="flex flex-col w-full items-center max-w-screen-sm mx-auto py-5 px-10 sm:px-16">
-      <h1 className="text-3xl font-bold my-5">Inforum</h1>
+      <Image src={logo} alt="logo" width={200} />
       <MyInfoForm userId={slug} isSignup={true} defaultTags={defaultTags} />
     </section>
   );
