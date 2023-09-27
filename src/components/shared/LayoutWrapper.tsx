3f8eac03d15f12846ed454b994e7b74894202aac
dongree <dongwoo0307@naver.com>
2023-09-27 12:51:53 +0900

    Chore: layout wrapprer default parameter 설정

--- src/components/shared/LayoutWrapper.tsx
+++ src/components/shared/LayoutWrapper.tsx
@@ -6,7 +6,11 @@ type Props = {
   bgColor?: string;
 };
 
-export default function LayoutWrapper({ children, paddingY, bgColor }: Props) {
+export default function LayoutWrapper({
+  children,
+  paddingY = 'sm:py-5',
+  bgColor = 'bg-white',
+}: Props) {
   return (
     <div className={`${bgColor} min-h-for-fit-screen h-full`}>
       <div className={`max-w-screen-xl mx-auto ${paddingY} sm:px-5`}>
