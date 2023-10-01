4fdb3638d2a617cfc0ff318931ceff7a458f0726
dongree <dongwoo0307@naver.com>
2023-10-02 03:06:43 +0900

    Chore: 팔로우 버튼 보이기 수정

--- src/components/me/Profile.tsx
+++ src/components/me/Profile.tsx
@@ -64,7 +64,7 @@ export default function Profile({
           ))}
         </div>
       )}
-      {isMine && (
+      {!isMine && (
         <FollowBtn
           isFollowed={userInfo.isFollowed}
           userId={userId}
