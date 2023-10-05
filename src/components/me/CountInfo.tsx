5d63258d7e15d7ff71a226cf6b2bc7b595d87298
dongree <dongwoo0307@naver.com>
2023-10-06 00:29:35 +0900

    Fix: 배포에 필요한 ui 추리기

--- src/components/me/CountInfo.tsx
+++ src/components/me/CountInfo.tsx
@@ -13,23 +13,23 @@ export default function CountInfo({ isMine }: Props) {
   return (
     <div className="flex text-center w-full border-y">
       <div
-        className="w-1/3 cursor-pointer transition-all hover:bg-soma-grey-30 py-4"
+        className="flex-1 cursor-pointer transition-all hover:bg-soma-grey-30 py-4"
         onClick={() => open('following')}
       >
         <p className="text-lg font-semibold">{currentFollowings.length}</p>
         <p className="text-xs text-gray-400">팔로잉</p>
       </div>
       <div
-        className="w-1/3 cursor-pointer transition-all hover:bg-soma-grey-30 py-4"
+        className="flex-1 cursor-pointer transition-all hover:bg-soma-grey-30 py-4"
         onClick={() => open('follower')}
       >
         <p className="text-lg font-semibold">{currentFollowers.length}</p>
         <p className="text-xs text-gray-400">팔로워</p>
       </div>
-      <div className="w-1/3 py-4">
+      {/* <div className="w-1/3 py-4">
         <p className="text-lg font-semibold">9999</p>
         <p className="text-xs text-gray-400">좋아요</p>
-      </div>
+      </div> */}
     </div>
   );
 }
