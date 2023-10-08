47984eb6d0e7f966c4e8c3d79219481e4448fc3b
dongree <dongwoo0307@naver.com>
2023-10-08 17:19:54 +0900

    Chore: 경로 이름 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default async function LandingPage() {
 
   const { isLogin } = await checkUser(cookie);
 
-  isLogin && redirect('/memo');
+  isLogin && redirect('/memos');
 
   return <LandingContainer />;
 }
