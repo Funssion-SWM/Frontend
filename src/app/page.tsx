3b321bde32589dc081aee51f70e2897d5b1f723f
dongree <dongwoo0307@naver.com>
2023-10-08 16:26:45 +0900

    Fix: landing footer와 일반 footer 배경색 구분, 랜딩페이지 UI 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,20 +9,12 @@ import editing from '@/assets/edit-memo.gif';
 import logo from '@/assets/inforum_logo.png';
 import editUnderline from '@/assets/icons/edit_underline.svg';
 import speechBubble from '@/assets/icons/speech_bubble.svg';
-import {
-  BsFillArrowDownCircleFill,
-  BsFillArrowUpCircleFill,
-} from 'react-icons/bs';
 import Footer from '@/components/shared/Footer';
+import down from '@/assets/icons/down.svg';
+
 export default function LandingPage() {
   const [readyState, setReadyState] = useState(false);
   const firstScreenRef = useRef<null | HTMLDivElement>(null);
-
-  const scrolltoTop = () => {
-    if (firstScreenRef.current) {
-      firstScreenRef.current.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
 
   return (
     <div className="snap-y snap-mandatory overflow-y-scroll h-screen">
@@ -30,11 +22,7 @@ export default function LandingPage() {
         className="flex relative flex-col justify-center items-center h-full sm:snap-always sm:snap-center  bg-gradient-to-t from-soma-blue-30 from-0% via-white via-80%"
         ref={firstScreenRef}
       >
-        <Image 
-          src={logo}
-          width={300}
-          alt="logo"
-        />
+        <Image src={logo} width={300} alt="logo" />
         <h1 className="text-4xl sm:text-5xl mt-5 font-bold text-center overflow-hidden">
           <TypeIt
             options={{
@@ -57,7 +45,7 @@ export default function LandingPage() {
           <p className="text-sm sm:text-2xl my-7 text-center ">
             자동 텍스트 생성 기능을 통해 글을 쉽고 빠르게 작성하세요
             <br />
-            파스텔톤 메모에 개발 기록을 간단하게 작성해보세요
+            블럭 기반 에디터를 활용해 개발 기록을 간편하게 작성해보세요
           </p>
           <Link href="/memos">
             <button className="bg-soma-blue-40 w-72 h-16 shadow-2xl text-white px-3.5 py-2 rounded-3xl transition hover:bg-soma-blue-50 sm:text-2xl">
@@ -65,8 +53,10 @@ export default function LandingPage() {
             </button>
           </Link>
         </div>
-        <BsFillArrowDownCircleFill
-          className={`absolute bottom-10 animate-bounce text-soma-blue-40 w-10 h-10 transition-opacity bg-white rounded-full
+        <Image
+          src={down}
+          alt="down"
+          className={`absolute bottom-10 animate-bounce transition-opacity rounded-full
         ${
           readyState ? 'opacity-100' : 'opacity-0 invisible'
         } duration-1000 ease-in-out duration`}
@@ -78,16 +68,20 @@ export default function LandingPage() {
           src={landing}
           alt="landing"
           width={600}
-          className="rounded-2xl py-2 px-4"
+          className="rounded-2xl"
         />
-        <div className='sm:ml-14 w-full sm:w-auto px-4'>
-          <Image src={speechBubble} alt='speech-bubble' className='w-[30px] sm:w-9 mt-5'/>
-          <p className='text-2xl sm:text-4xl font-bold leading-tight mb-5'>
+        <div className="sm:ml-14 w-full sm:w-auto px-4">
+          <Image
+            src={speechBubble}
+            alt="speech-bubble"
+            className="w-[30px] sm:w-9 mt-5"
+          />
+          <p className="text-2xl sm:text-4xl font-bold leading-tight mb-5">
             Gen AI를 활용한
             <br />
             텍스트 자동 생성 기능
           </p>
-          <p className='text-sm sm:text-xl font-normal'>
+          <p className="text-sm sm:text-xl font-normal">
             작성 중 ++를 입력해 텍스트를 자동 생성해보세요!
             <br />
             검색보다 빠르고 간편하게 정보를 제공합니다.
@@ -96,29 +90,36 @@ export default function LandingPage() {
       </div>
 
       <div className="flex flex-col-reverse sm:flex-row items-center justify-center h-full font-semibold sm:snap-always sm:snap-center ">
-        <div className='sm:mr-14 w-full sm:w-auto px-4'>
-          <Image src={editUnderline} alt='edit-underline' className='w-[30px] sm:w-9 mt-5'/>
-          <p className='text-2xl sm:text-4xl font-bold leading-tight mb-5'>
-            Notion Like Editor를 활용한<br />
+        <div className="sm:mr-14 w-full sm:w-auto px-4">
+          <Image
+            src={editUnderline}
+            alt="edit-underline"
+            className="w-[30px] sm:w-9 mt-5"
+          />
+          <p className="text-2xl sm:text-4xl font-bold leading-tight mb-5">
+            블럭 기반 에디터를 활용한
+            <br />
             간편한 에디팅 기능
           </p>
-          <p className='text-sm sm:text-xl font-normal'>
-            바로 반영되는 마크다운을 사용해보세요!<br />
-            / 키와 드래그를 이용하여 다양한 편집 기능을 사용해보세요!
+          <p className="text-sm sm:text-xl font-normal">
+            바로 반영되는 마크다운을 사용해보세요!
+            <br />/ 키와 드래그를 이용하여 다양한 편집 기능을 사용해보세요!
           </p>
         </div>
         <Image
           src={editing}
           alt="landing"
           width={600}
-          className="rounded-2xl py-2 px-4"
+          className="rounded-2xl"
         />
       </div>
 
       <div className="flex relative flex-col items-center justify-center h-full font-semibold sm:snap-always sm:snap-center bg-[#F6F8FC]">
-        <p className='text-4xl sm:text-5xl w-80 sm:w-auto font-bold text-center leading-snug sm:leading-snug mb-16'>
-          인포럼과 함께<br />
-          토론하며 성장하는<br />
+        <p className="text-4xl sm:text-5xl w-80 sm:w-auto font-bold text-center leading-snug sm:leading-snug mb-16">
+          인포럼과 함께
+          <br />
+          토론하며 성장하는
+          <br />
           긍정적인 개발공간을 만들어보세요.
         </p>
         <Link href="/memos">
@@ -128,7 +129,7 @@ export default function LandingPage() {
         </Link>
       </div>
 
-      <Footer extraClass='w-full sm:snap-always sm:snap-end'/>
+      <Footer extraClass="w-full sm:snap-always sm:snap-end bg-white" />
 
       {/* <div className="h-full relative flex flex-col items-center justify-center snap-always snap-center bg-[#F6F8FC]">
         <p className="text-lg sm:text-2xl font-semibold">지원 예정</p>
--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -5,55 +5,63 @@ import kakao from '@/assets/icons/kakao.svg';
 
 type Props = {
   extraClass?: string;
-}
+};
 
-export default function Footer({extraClass}:Props) {
+export default function Footer({ extraClass }: Props) {
   return (
-    <div className={`${extraClass}`}>
-      <footer className="flex flex-col sm:flex-row items-center pt-10 px-4 sm:max-w-screen-xl m-auto text-soma-grey-70 text-sm">
-        <div className='flex flex-col w-full sm:w-auto'>
-          <p>
-            <Image src={logo} alt="logo" width={110} className='mr-7 mb-5 sm:mb-0 sm:inline-block'/>
-            <span className='font-bold'>이용약관 및 개인정보처리방침</span>
+    <footer className={`bg-soma-grey-25 py-10 ${extraClass}`}>
+      <div className="flex flex-col sm:flex-row items-center px-4 sm:max-w-screen-xl m-auto text-soma-grey-70 text-xs sm:text-sm">
+        <div className="flex flex-col w-full sm:w-auto">
+          <p className="flex items-center">
+            <Image
+              src={logo}
+              alt="logo"
+              width={110}
+              className="mr-7 sm:mb-0 sm:inline-block"
+            />
+            <div className="flex gap-5">
+              <button className="font-bold">이용약관</button>
+              <button className="font-bold">개인정보 처리 방침</button>
+            </div>
           </p>
-          <p className='mt-5 sm:mt-10'>
-            <span className='font-bold mr-2'>Add.</span>
-            <span>서울 특별시 강남구 테헤란로 311 SW 마에스트로 연수센터</span>
+          <p className="mt-5 sm:mt-10">
+            <span className="font-bold mr-2">Add.</span>
+            <span>서울특별시 강남구 테헤란로 311 SW마에스트로 연수센터</span>
           </p>
-          <p className='mt-5 sm:mt-3'>
-            <span className='font-bold mr-2'>Tel.</span>
+          <p className="mt-5 sm:mt-3">
+            <span className="font-bold mr-2">Tel.</span>
             <Link
-              href="https://open.kakao.com/o/gbdqT3Af"
+              href="http://pf.kakao.com/_WxdeCG"
               className="text-soma-blue-50"
             >
-              카카오톡 오픈 채팅방
+              카카오톡 채널
             </Link>
           </p>
         </div>
 
-        <div className='flex flex-col sm:ml-20 w-full sm:w-auto'>
-          <p className='mt-5 sm:mt-20'>
-            <span className='font-bold mr-2'>Mail.</span>
+        <div className="flex flex-col sm:ml-20 w-full sm:w-auto">
+          <p className="mt-5 sm:mt-20">
+            <span className="font-bold mr-2">Mail.</span>
             <span>dongwoo0307@naver.com</span>
           </p>
-          <p className='mt-5 sm:mt-3'>
-            <span className='font-bold mr-2'>문의 구글폼</span>
+          <p className="mt-5 sm:mt-3">
+            <span className="font-bold mr-2">문의 구글폼</span>
             <span>?</span>
           </p>
         </div>
 
-        <div className='hidden sm:block mt-20 ml-auto'>
+        <div className="hidden sm:block mt-20 ml-auto">
           <Link
-            href="https://open.kakao.com/o/gbdqT3Af"
+            href="http://pf.kakao.com/_WxdeCG"
             className="text-soma-blue-50"
           >
-            <Image src={kakao} alt='kakao' width={24} />
+            <Image src={kakao} alt="kakao" width={24} />
           </Link>
         </div>
-      </footer>
-      <p className='text-base py-5 sm:my-10 px-4 text-[#888] max-w-screen-xl sm:mx-auto font-normal w-72 sm:w-auto mr-auto'>
+      </div>
+      <p className="text-sm sm:text-base mt-5 sm:mt-10 px-4 text-[#888] max-w-screen-xl sm:mx-auto font-normal w-72 sm:w-auto mr-auto">
         Copyright © 2023 SWM Funssion() Team All rights reserved.
       </p>
-    </div>
+    </footer>
   );
 }
