3d6312369a585c0d595ddab141acea935091be13
dongree <dongwoo0307@naver.com>
2023-10-09 19:01:09 +0900

    Fix: referer로 외부 다이렉트 접근 막기

--- src/app/create/memo/page.tsx
+++ src/app/create/memo/page.tsx
@@ -1,6 +1,8 @@
 import EditorForm from '@/components/create/memo/EditorForm';
 import LayoutWrapper from '@/components/shared/LayoutWrapper';
 import { Metadata } from 'next';
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 export const metadata: Metadata = {
   title: '메모 작성',
@@ -8,6 +10,10 @@ export const metadata: Metadata = {
 };
 
 export default function CreateMemoPage() {
+  const headersList = headers();
+  const referer = headersList.get('referer');
+  referer === null && redirect('/memos');
+
   return (
     <LayoutWrapper paddingY="sm:py-10">
       <EditorForm />
