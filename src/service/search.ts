4291edb8d2146246ff18ccadc2adb5f94fc2bfb8
comolove <jungjinwoo41@gmail.com>
2023-10-10 13:30:55 +0900

    feat: 질문 검색 기능 추가

--- src/service/search.ts
+++ src/service/search.ts
@@ -1,4 +1,6 @@
-import { SearchHistory } from '@/types';
+import { Orderby, SearchHistory } from '@/types';
+import { Memo } from '@/types/memo';
+import { Question } from '@/types/question';
 import { ACCESS_TOKEN } from '@/utils/const';
 
 export async function addSearchHistory(
@@ -56,4 +58,54 @@ export async function refreshSearchHistory(historyId: number): Promise<void> {
   ).then((res) => {
     if (!res.ok) throw new Error('search history error 발생!');
   });
+}
+
+export async function searchMemos(
+  searchString: string,
+  orderBy: Orderby,
+  isTag: Boolean,
+  userId: string
+): Promise<Memo[]> {
+  const url = new URL(
+    `${process.env.NEXT_PUBLIC_SERVER_IP_ADDRESS}/memos/search`
+  );
+  const params = {
+    searchString: searchString,
+    orderBy: orderBy,
+    isTag: isTag.toString(),
+    userId: userId,
+  };
+  url.search = new URLSearchParams(params).toString();
+
+  return fetch(url, { next: { revalidate: 0 } })
+    .then((res) => {
+      if (!res.ok) throw new Error('error 발생!');
+      return res.json();
+    })
+    .catch(console.error);
+}
+
+export async function searchQuestions(
+  searchString: string,
+  orderBy: Orderby,
+  isTag: Boolean,
+  userId: string
+): Promise<Question[]> {
+  const url = new URL(
+    `${process.env.NEXT_PUBLIC_SERVER_IP_ADDRESS}/questions/search`
+  );
+  const params = {
+    searchString: searchString,
+    orderBy: orderBy,
+    isTag: isTag.toString(),
+    userId: userId,
+  };
+  url.search = new URLSearchParams(params).toString();
+
+  return fetch(url, { next: { revalidate: 0 } })
+    .then((res) => {
+      if (!res.ok) throw new Error('error 발생!');
+      return res.json();
+    })
+    .catch(console.error);
 }
