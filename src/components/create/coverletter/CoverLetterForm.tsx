5297114f7c8b98ece37b4d4b3e24737049b2c767
dongree <dongwoo0307@naver.com>
2023-10-27 20:30:27 +0900

    Feat: 자소서 기술스택 추가 기능 구현, 레벨 정보란 추가

--- src/components/create/coverletter/CoverLetterForm.tsx
+++ src/components/create/coverletter/CoverLetterForm.tsx
@@ -1,37 +1,138 @@
 'use client';
 
 import BlueBtn from '@/components/shared/btn/BlueBtn';
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
+import { AiOutlineInfoCircle } from 'react-icons/ai';
 
 const TITLE_STYLE = 'text-lg font-semibold';
 
 const TEXTAREA_STYLE =
   'bg-soma-grey-20 w-full resize-none h-36 text-lg break-all p-4 outline-none rounded-xl';
 
+type StackInfo = {
+  stack: string;
+  level: number;
+};
+
+const stackOptions = ['Java', 'JavaScript', 'Python', 'C', 'C++'];
+const levelOptions = [0, 1, 2, 3, 4, 5];
+
 export default function CoverLetterForm() {
+  const [stackInfos, setStackInfos] = useState<StackInfo[]>([
+    { stack: 'JavaScript', level: 5 },
+  ]);
+  const [stackInfo, setStackInfo] = useState<StackInfo>({
+    stack: stackOptions[0],
+    level: levelOptions[0],
+  });
   const [answer1, setAnswer1] = useState('');
   const [answer2, setAnswer2] = useState('');
   const [answer3, setAnswer3] = useState('');
 
+  const handleOptionsChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    const { value, name } = e.target;
+    setStackInfo((info) => {
+      return { ...info, [name]: value };
+    });
+  };
+
+  const handleAddStackInfo = () => {
+    setStackInfos((preInfos) => [...preInfos, stackInfo]);
+    setStackInfo({
+      stack: stackOptions[0],
+      level: levelOptions[0],
+    });
+  };
+
   return (
     <div>
       <div className="mb-5">
-        <div className={TITLE_STYLE}>
-          기술스택 레벨 (자신있는 기술스택의 레벨을 정의해주세요.)
+        <div className="flex items-center">
+          <div className={TITLE_STYLE}>
+            기술스택 레벨 (자신있는 기술스택의 레벨을 정의해주세요. 레벨정의는
+            우측 정보란에 나와있습니다.)
+          </div>
+          <div className="group mx-2 relative">
+            <AiOutlineInfoCircle />
+            <div className="absolute top-0 left-0 w-[460px] p-3 bg-white opacity-0 pointer-events-none transition-all group-hover:opacity-100 group-hover:pointer-events-auto rounded-2xl shadow-lg">
+              <ul className="flex flex-col text-sm gap-3">
+                <li> 0: 개발 경험이나 기술 지식이 전혀 없음.</li>
+                <li>
+                  1: 관련 지식이나 경험이 적고, 간단한 기능 구현에도 책/인터넷의
+                  도움이 필요함
+                </li>
+                <li>
+                  2: 능숙하진 않으나, 코드를 이해하고 필수적인 기능을 구현할 수
+                  있음.
+                </li>
+                <li>
+                  3: 시스템의 기본 동작 방식을 이해하고, 필수적인 기능을
+                  구현하는 데 어려움이 없음. 필요한 경우 참고 자료를 활용하여
+                  작업을 진행할 수 있음.
+                </li>
+                <li>
+                  4: 관련 지식과 경험이 많고, 중간 규모의 시스템을 개발할 수
+                  있으며, 기술적인 문제가 발생했을 때 이해하고 해결할 수 있는
+                  능력이 있음
+                </li>
+                <li>
+                  5: 규모가 크고 복잡한 소프트웨어 및 시스템을 개발할 수 있으며,
+                  기술적 도전을 독립적으로 해결할 수 있는 능력이 있음. 다른
+                  사람을 리드하고 교육할 수 있음.
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-        <div className="flex gap-5">
-          <div className="flex gap-2">
-            <div>JavaScript</div>
-            <div>level5</div>
+
+        <div>
+          <div className="flex">
+            <div>
+              <label>Stack :</label>
+              <select
+                className="outline-none text-center"
+                value={stackInfo.stack}
+                name="stack"
+                onChange={handleOptionsChange}
+              >
+                {stackOptions.map((option, idx) => (
+                  <option key={idx} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div>
+              <label>Level :</label>
+              <select
+                className="outline-none text-center"
+                value={stackInfo.level}
+                name="level"
+                onChange={handleOptionsChange}
+              >
+                {levelOptions.map((level, idx) => (
+                  <option key={idx} value={level}>
+                    {level}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <button
+              className="bg-soma-blue-40 text-white w-6 h-6 rounded-lg"
+              onClick={handleAddStackInfo}
+            >
+              +
+            </button>
           </div>
-          <div className="flex gap-2">
-            <div>TypeScript</div>
-            <div>level5</div>
-          </div>
-          <div className="flex gap-2">
-            <div>JavaScript</div>
-            <div>level5</div>
-          </div>
+
+          <ul className="flex gap-5">
+            {stackInfos.map((item, idx) => (
+              <div key={idx} className="flex gap-2">
+                <p>{item.stack} :</p>
+                <p>{item.level}</p>
+              </div>
+            ))}
+          </ul>
         </div>
       </div>
 
