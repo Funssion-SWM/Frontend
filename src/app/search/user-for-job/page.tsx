4a69caa1c43c5eb058f917807cf1beda46a097d3
dongree <dongwoo0307@naver.com>
2023-11-16 00:24:43 +0900

    Chore: referer를 이용해 url 직접 접근 차단

--- src/app/search/user-for-job/page.tsx
+++ src/app/search/user-for-job/page.tsx
@@ -1,7 +1,14 @@
 import SearchUserForJobContainer from '@/components/search/user-for-job/SearchUserForJobContainer';
 import LayoutWrapper from '@/components/shared/LayoutWrapper';
+import { MAIN_PATH } from '@/utils/const';
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 export default function SearchUserForJobPage() {
+  const headersList = headers();
+  const referer = headersList.get('referer');
+  referer === null && redirect(MAIN_PATH);
+
   return (
     <section>
       <LayoutWrapper paddingY="sm:py-0">
