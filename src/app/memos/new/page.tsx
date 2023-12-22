bfe0e7d835437af691b1bd5a258182aadf0ee5fb
dongree <dongwoo0307@naver.com>
2023-12-22 16:31:19 +0900

    Fix: Link를 사용해 API 요청횟수 최소화: Memo

---
+++ src/app/memos/new/page.tsx
@@ -0,0 +1,14 @@
+import MemosContainer from '@/components/memo/MemosContainer';
+import { getMemos } from '@/service/memos';
+import { MEMO_NUMBER_PER_PAGE_FOR_INFINITY_SCROLL } from '@/utils/const';
+
+export default async function MemosNewPage() {
+  const memos = await getMemos(
+    'month',
+    'new',
+    0,
+    MEMO_NUMBER_PER_PAGE_FOR_INFINITY_SCROLL
+  );
+
+  return <MemosContainer memos={memos} type="new" />;
+}
--- src/app/memos/page.tsx
+++ src/app/memos/page.tsx
@@ -1,59 +1,14 @@
 import MemosContainer from '@/components/memo/MemosContainer';
-import Footer from '@/components/shared/Footer';
-import Header from '@/components/shared/Header';
-import LayoutWrapper from '@/components/shared/LayoutWrapper';
-import { checkUser, getUserInfo } from '@/service/auth';
 import { getMemos } from '@/service/memos';
-import { getNotificationsTop30 } from '@/service/notification';
-import {
-  ACCESS_TOKEN,
-  MEMO_NUMBER_PER_PAGE_FOR_INFINITY_SCROLL,
-  REFRESH_TOKEN,
-} from '@/utils/const';
-import { Metadata } from 'next';
-import { cookies } from 'next/headers';
-
-export const metadata: Metadata = {
-  title: '인포럼 - Memos',
-  description: '인포럼 Memos 페이지입니다.',
-  keywords: ['inforum', '인포럼', 'memo', '메모', '블로그', 'blog'],
-};
+import { MEMO_NUMBER_PER_PAGE_FOR_INFINITY_SCROLL } from '@/utils/const';
 
 export default async function MemosPage() {
-  const accessToken = cookies().get(ACCESS_TOKEN)?.value;
-  const refreshToken = cookies().get(REFRESH_TOKEN)?.value;
-  const cookie = `${ACCESS_TOKEN}=${accessToken}; ${REFRESH_TOKEN}=${refreshToken}`;
-
-  const memosData = getMemos(
+  const memos = await getMemos(
     'month',
     'new',
     0,
     MEMO_NUMBER_PER_PAGE_FOR_INFINITY_SCROLL
   );
-  const myData = checkUser(cookie);
-  const [memos, { id, isLogin, authority }] = await Promise.all([
-    memosData,
-    myData,
-  ]);
 
-  const { profileImageFilePath } = isLogin
-    ? await getUserInfo(id)
-    : { profileImageFilePath: undefined };
-
-  const notifications = isLogin ? await getNotificationsTop30(cookie) : [];
-
-  return (
-    <section>
-      <Header
-        isLogin={isLogin}
-        notifications={notifications}
-        profileImageFilePath={profileImageFilePath}
-        currentPage="memos"
-        authority={authority}
-      />
-      <LayoutWrapper paddingY="sm:py-5">
-        <MemosContainer memos={memos} />
-      </LayoutWrapper>
-    </section>
-  );
+  return <MemosContainer memos={memos} type="new" />;
 }
